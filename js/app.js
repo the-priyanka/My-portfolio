e8e741072a6a4e013f54fa4b2303e1c7cae68512
Priyanka <original.manaria@gmail.com>
2021-09-16 16:29:41 +0530

    set theme localStorage

--- js/app.js
+++ js/app.js
@@ -90,11 +90,23 @@ const toggleSwitch = document.querySelector("input[type='checkbox']")
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark")
+    localStorage.setItem("theme", "dark")
   }
   else {
     document.documentElement.setAttribute("data-theme", "light")
+    localStorage.setItem("theme", "light")
   }
 }
 
 // event listener 
 toggleSwitch.addEventListener('change', switchTheme)
+
+// Check localStorage for theme 
+const currentTheme = localStorage.getItem("theme")
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme)
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true
+    darkMode()
+  }
+}
